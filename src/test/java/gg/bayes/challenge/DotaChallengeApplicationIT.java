982170a16f3ffacd0a9af19864cf5b5c83bcac9e
daniel-kollanyi <daniel.kollanyi@falcon.io>
2020-08-02 13:14:43 +0200

    Configure Postgres DB, add docker-compose files for development and testing

--- src/test/java/gg/bayes/challenge/DotaChallengeApplicationIT.java
+++ src/test/java/gg/bayes/challenge/DotaChallengeApplicationIT.java
@@ -2,9 +2,11 @@ package gg.bayes.challenge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class DotaChallengeApplicationTests {
+@ActiveProfiles("it")
+class DotaChallengeApplicationIT {
 
     @Test
     void contextLoads() {
